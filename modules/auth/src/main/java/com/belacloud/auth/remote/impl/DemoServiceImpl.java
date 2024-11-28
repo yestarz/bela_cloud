e521be38710e17086b4b212fd536ced2fa36d484
yancy <981987024@qq.com>
2024-11-28 17:12:41 +0800

    Update DemoServiceImpl.java

--- modules/auth/src/main/java/com/belacloud/auth/remote/impl/DemoServiceImpl.java
+++ modules/auth/src/main/java/com/belacloud/auth/remote/impl/DemoServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 @RefreshScope
 public class DemoServiceImpl implements DemoService {
 
+    private Integer age;
+    
     @Value("${app.client-id}")
     private String clientId;
 
