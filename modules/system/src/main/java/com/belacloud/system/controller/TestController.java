42e924552b8346055124748854ddfbefc20681c5
yangxin <981987024@qq.com>
2024-12-02 14:33:22 +0800

    整合SaToken

--- modules/system/src/main/java/com/belacloud/system/controller/TestController.java
+++ modules/system/src/main/java/com/belacloud/system/controller/TestController.java
@@ -1,11 +1,14 @@
 package com.belacloud.system.controller;
 
 import com.belacloud.api.demo.service.DemoService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Enumeration;
 
 @RestController
 @RequestMapping("/test")
@@ -15,7 +18,12 @@ public class TestController {
     private DemoService demoService;
 
     @GetMapping("/test")
-    public String get(@RequestParam(name = "name") String name){
+    public String get(@RequestParam(name = "name") String name, HttpServletRequest request){
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            System.out.println(headerName + ":" + request.getHeader(headerName));
+        }
         return demoService.sayHello(name);
     }
 }
